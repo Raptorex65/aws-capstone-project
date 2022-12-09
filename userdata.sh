45ccade637562923384db2d250c95e2d296d901a
Raptorex65 <lamek3565@gmail.com>
2022-12-09 18:49:51 +0100

    userdata changed

--- userdata.sh
+++ userdata.sh
@@ -3,9 +3,9 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-TOKEN="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"
-git clone https://$TOKEN@github.com/Raptorex65/my-aws-capstone-project.git
-cd /home/ubuntu/my-aws-capstone-project
+TOKEN="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"
+git clone https://$TOKEN@github.com/Raptorex65/aws-capstone-project.git
+cd /home/ubuntu/aws-capstone-project
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
 pip3 install -r requirements.txt
